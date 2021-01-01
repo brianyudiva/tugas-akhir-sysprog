b2f0cd262ef05cdb00d063e389895a17ae13ba57
brianyudiva <brian.yudiva@gmail.com>
2021-01-02 02:20:49 +0700

    no need to set up default printer

--- printer/views.py
+++ printer/views.py
@@ -5,19 +5,20 @@ from django.conf import settings
 import subprocess
 
 def index(request):
+  printer = subprocess.check_output(['lpstat','-p']).decode().split()[1]
   if request.method == 'POST':
     form = UploadFileForm(request.POST, request.FILES)
     if form.is_valid():
-      call_bash(request.FILES['file'])
+      call_bash(request.FILES['file'], printer)
   else:
     form = UploadFileForm()
-  return render(request, 'index.html', {'form' : form})
+  return render(request, 'index.html', {'form' : form, 'printer' : printer })
 
-def call_bash(file):
+def call_bash(file, print):
   fs = FileSystemStorage()
   filename = fs.save(file.name, file)
   abs_path = settings.MEDIA_ROOT + "/" + filename
-  subprocess.Popen(['./printer.sh', abs_path])
+  subprocess.Popen(['lp','-d',print,abs_path])
   return
 
 
