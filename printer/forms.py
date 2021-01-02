ceccbe3a7acb73f6e03a05bb0a8adbb93f8ada4e
mhuda1 <mushaffahuda1@gmail.com>
2021-01-02 08:06:08 +0000

    adding printer options

--- printer/forms.py
+++ printer/forms.py
@@ -1,5 +1,27 @@
 from django import forms
+import subprocess
+
+COLOR = [
+    ('RGB', 'Color'),
+    ('Gray', 'Grayscale'),
+]
+SIZE = [
+    ('A4', 'A4'),
+    ('Letter', 'Letter'),
+]
+QUALITY = [
+    ('Draft', 'Draft'),
+    ('Normal', 'Normal'),
+    ('High', 'High'),
+]
+
 
 class UploadFileForm(forms.Form):
     file = forms.FileField(label='')
-    file.widget.attrs['class'] = 'form-control form-control-lg shadow'
+    file.widget.attrs['class'] = 'form-control form-control-lg shadow mb-3'
+    color = forms.CharField(label='Color Mode: ',
+                            widget=forms.Select(choices=COLOR))
+    printQuality = forms.CharField(
+        label='Print Quality: ', widget=forms.Select(choices=QUALITY))
+    pageSize = forms.CharField(
+        label='Page size: ', widget=forms.Select(choices=SIZE))
--- printer/views.py
+++ printer/views.py
@@ -4,21 +4,31 @@ from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 import subprocess
 
+
 def index(request):
-  printer = subprocess.check_output(['lpstat','-p']).decode().split()[1]
-  if request.method == 'POST':
-    form = UploadFileForm(request.POST, request.FILES)
-    if form.is_valid():
-      call_bash(request.FILES['file'], printer)
-  else:
-    form = UploadFileForm()
-  return render(request, 'index.html', {'form' : form, 'printer' : printer })
+    printer = subprocess.check_output(['lpstat', '-p']).decode().split()[1]
+    options = subprocess.check_output(
+        ['lpoptions', '-l']).decode().split()
+    print(options)
+    if request.method == 'POST':
+        form = UploadFileForm(request.POST, request.FILES)
+        if form.is_valid():
+            color_mode = request.POST.get('color', 'RGB')
+            print_quality = request.POST['printQuality']
+            page_size = request.POST['pageSize']
+            print(color_mode, print_quality, page_size)
+            call_bash(request.FILES['file'], printer, color_mode, print_quality, page_size)
+    else:
+        form = UploadFileForm()
+    return render(request, 'index.html', {'form': form, 'printer': printer})
 
-def call_bash(file, print):
-  fs = FileSystemStorage()
-  filename = fs.save(file.name, file)
-  abs_path = settings.MEDIA_ROOT + "/" + filename
-  subprocess.Popen(['lp','-d',print,abs_path])
-  return
 
-
+def call_bash(file, print, color_mode, print_quality, page_size):
+    fs = FileSystemStorage()
+    filename = fs.save(file.name, file)
+    abs_path = settings.MEDIA_ROOT + "/" + filename
+    color = "ColorModel=" + color_mode
+    quality = "Print=" + print_quality
+    size = "PageSize=" + page_size
+    subprocess.Popen(['lp', '-d', print, '-o', color, '-o', quality, '-o', size, abs_path])
+    return
